529290c3560537dac792092fc0533c129b12a7f5
Azeez <aziz.khadjayev070@gmail.com>
2018-09-27 18:54:50 +0200

    commit

---
+++ js/about.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    //Get full year
+    $('#year').text(new Date().getFullYear());
+    /* back to top */
+	var toBack = $('#toTop');
+	$(window).on('scroll', function(){
+		if($(window).scrollTop() >= 100){
+			toBack.fadeIn();
+		}else{
+			toBack.fadeOut();
+		}
+	});
+	toBack.on('click', function(){
+		$('html, body').animate({scrollTop:0},1000)
+    });
+    //slick
+    $('.slider').slick({
+        infinite: true,
+        slideToShow: 1,
+        slideToScroll: 1
+    });
+});
---
+++ js/contact.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    //Get full year
+    $('#year').text(new Date().getFullYear());
+    /* back to top */
+	var toBack = $('#toTop');
+	$(window).on('scroll', function(){
+		if($(window).scrollTop() >= 100){
+			toBack.fadeIn();
+		}else{
+			toBack.fadeOut();
+		}
+	});
+	toBack.on('click', function(){
+		$('html, body').animate({scrollTop:0},1000)
+    });
+});
